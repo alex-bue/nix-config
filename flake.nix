809455c405f98d13d1a0dc4c3f6c3720cba87522
Alexander Büscher <alexanderbuescher@outlook.com>
2026-08-26 22:01:25 +0200

    update flake inputs

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
     };
 
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
+      url = "github:noctalia-dev/noctalia-shell/v4.7.7";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.noctalia-qs.follows = "noctalia-qs";
     };
