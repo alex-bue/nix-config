de650082810db34217748e01631882ccb678c7c4
Alexander Büscher <alexanderbuescher@outlook.com>
2026-08-30 12:50:54 +0200

    install lazyvim runtime dependencies

--- modules/development/neovim.nix
+++ modules/development/neovim.nix
@@ -1,5 +1,18 @@
 {
-  flake.modules.homeManager.neovim = { pkgs, ... }: {
-    home.packages = [ pkgs.neovim ];
+  flake.modules.homeManager.neovim = { lib, pkgs, ... }: {
+    home.packages =
+      with pkgs;
+      [
+        curl
+        fd
+        fzf
+        git
+        lazygit
+        neovim
+        ripgrep
+        stdenv.cc
+        tree-sitter
+      ]
+      ++ lib.optionals stdenv.hostPlatform.isLinux [ wl-clipboard ];
   };
 }
